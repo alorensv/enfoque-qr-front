ac7b7aa4042cbc8b9b1002c314451226ac8b5896
Alorensv <alorensv@gmail.com>
2026-02-03 11:57:24 -0300

    Correción ruta con variables de entorno para LOGIN
    Pendiente corregir otras rutas con la variable de entorno.

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,8 @@ export default function Home() {
     const email = form.email.value;
     const password = form.password.value;
     try {
-      const res = await fetch('http://localhost:3001/auth/login', {
+      const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+      const res = await fetch(`${apiUrl}/auth/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password })
